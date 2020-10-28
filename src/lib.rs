7b3c64470456fb2f6d051f28372405a594a4f1ad
Rita Kitic <rikitau@protonmail.com>
2020-10-28 17:17:08 +0100

    add license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+// Rust implementation of bip-85
+// Written in 2020 by
+//     Rita Kitic <rikitau@protonmail.com>
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication
+// along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+
 //! # BIP-85 deterministic entropy generation
 
 #![deny(non_upper_case_globals)]
