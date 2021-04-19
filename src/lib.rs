55b399debad34e62eaee363765cccec7e00d06d4
Rita Kitic <rikitau@protonmail.com>
2021-04-19 16:26:29 +0200

    re-export bitcoin and bip39

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,6 @@
 // Rust implementation of bip-85
 // Written in 2020 by
 //     Rita Kitic <rikitau@protonmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all
-// copyright and related and neighboring rights to this software to
-// the public domain worldwide. This software is distributed without
-// any warranty.
-//
-// You should have received a copy of the CC0 Public Domain Dedication
-// along with this software.
-// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-
 
 //! # BIP-85 deterministic entropy generation
 //!
@@ -38,9 +28,9 @@
 #![deny(unused_imports)]
 #![deny(missing_docs)]
 
-extern crate bitcoin;
+pub extern crate bitcoin;
 #[cfg(feature = "mnemonic")]
-extern crate bip39;
+pub extern crate bip39;
 
 use std::fmt;
 use std::default::Default;
